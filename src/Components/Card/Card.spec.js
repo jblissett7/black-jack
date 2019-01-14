b917f5147bcc071ab49b30180590b68107b190a5
jblissett7 <jblissett7@gmail.com>
2019-01-14 16:05:02 -0600

    Create Card Component and test files

---
+++ src/Components/Card/Card.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from './Card';
+
+describe('Card', () => {
+  let wrapper;
+  beforeEach(() => (wrapper = shallow(<Card />)));
+
+  it('Should render a <div />', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+});
